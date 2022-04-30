7488e6185fd2cba8706cb7448dd985b54eb0399b
Nguyen Minh <minhjavapc@gmail.com>
2022-04-30 16:36:54 +0700

    spring boot

---
+++ src/test/java/com/mb/mbe/MbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.mbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
