0f232bed807d321b837dd98e68344e35fbf7bc8d
Nguyen Minh <minhjavapc@gmail.com>
2022-05-02 23:26:58 +0700

    layouts

--- src/main/java/com/mb/mbe/controller/web/HomeController.java
+++ src/main/java/com/mb/mbe/controller/web/HomeController.java
@@ -2,20 +2,18 @@ package com.mb.mbe.controller.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-@RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("/")
     public String getHome() {
-        return "home";
-    }
-
-    @GetMapping("/h")
-    public String getHouse() {
         return "index";
     }
 
+    @GetMapping("/{name}")
+    public String getHome(@PathVariable("name") String name) {
+        return name;
+    }
 }
