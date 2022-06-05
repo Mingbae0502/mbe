a237f173791e20b7cef34097b8d4ef3f914da5c0
minhjavapc <104562096+Mingbae0502@users.noreply.github.com>
2022-06-05 23:05:16 +0700

    udpate

---
+++ src/main/java/com/mb/mbe/repository/Nhenhang.java
@@ -0,0 +1,8 @@
+package com.mb.mbe.repository;
+
+import com.mb.mbe.entity.Artist;
+import org.springframework.data.mirage.repository.MirageRepository;
+
+public interface Nhenhang extends MirageRepository<Artist, Long> {
+
+}
